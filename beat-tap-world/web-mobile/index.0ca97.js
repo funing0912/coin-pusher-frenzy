2de607ae3b00f7a271f3e541de1ac9616b335da6
FuNing <funing0721b@live.com>
2026-09-03 11:46:43 +0800

    骆驼关卡改为三点站位；增加氛围组

--- beat-tap-world/web-mobile/index.0ca97.js
+++ beat-tap-world/web-mobile/index.0ca97.js
@@ -1,4 +1,4 @@
-System.register(["./application.ad429.js"], function (_export, _context) {
+System.register(["./application.a47ae.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
