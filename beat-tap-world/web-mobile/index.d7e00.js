2b26231b6b42c9159bf4d15053172e787b86a3cb
FuNing <funing0721b@live.com>
2026-09-07 19:38:38 +0800

    为 player 增加 run_dust 和 speed_line，player 支持空 dush

--- beat-tap-world/web-mobile/index.d7e00.js
+++ beat-tap-world/web-mobile/index.d7e00.js
@@ -1,4 +1,4 @@
-System.register(["./application.741ab.js"], function (_export, _context) {
+System.register(["./application.d22fb.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
