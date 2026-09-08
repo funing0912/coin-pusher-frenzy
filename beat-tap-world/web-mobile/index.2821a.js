a49fb74adc0db751ba0883f19ae573fc20512d9d
FuNing <funing0721b@live.com>
2026-09-08 18:49:19 +0800

    更新骆驼图集资源

--- beat-tap-world/web-mobile/index.2821a.js
+++ beat-tap-world/web-mobile/index.2821a.js
@@ -1,4 +1,4 @@
-System.register(["./application.d22fb.js"], function (_export, _context) {
+System.register(["./application.fc478.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
