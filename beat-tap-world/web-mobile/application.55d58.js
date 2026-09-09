62f2d5cde3ac2ec721cf92c642b5953815096f19
FuNing <funing0721b@live.com>
2026-09-09 23:13:46 +0800

    chart 支持节奏型；每次 prep 都有音效

--- beat-tap-world/web-mobile/application.55d58.js
+++ beat-tap-world/web-mobile/application.55d58.js
@@ -14,7 +14,7 @@ System.register([], function (_export, _context) {
       _export("Application", Application = /*#__PURE__*/function () {
         function Application() {
           _classCallCheck(this, Application);
-          this.settingsPath = 'src/settings.a2fed.json';
+          this.settingsPath = 'src/settings.ab7e3.json';
           this.showFPS = false;
         }
         _createClass(Application, [{
