8878c499c9980a3141240926254fc40e2a477212
FuNing <funing0721b@live.com>
2026-09-10 20:58:20 +0800

    调整 miss 动画帧时长；新增连击提示数字

--- beat-tap-world/web-mobile/application.bd556.js
+++ beat-tap-world/web-mobile/application.bd556.js
@@ -14,7 +14,7 @@ System.register([], function (_export, _context) {
       _export("Application", Application = /*#__PURE__*/function () {
         function Application() {
           _classCallCheck(this, Application);
-          this.settingsPath = 'src/settings.42501.json';
+          this.settingsPath = 'src/settings.62d03.json';
           this.showFPS = false;
         }
         _createClass(Application, [{
--- beat-tap-world/web-mobile/index.e567f.js
+++ beat-tap-world/web-mobile/index.e567f.js
@@ -1,4 +1,4 @@
-System.register(["./application.e4353.js"], function (_export, _context) {
+System.register(["./application.bd556.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
