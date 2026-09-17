fba7e8b87b7c59341bc0f7cb596552703608839c
FuNing <funing0721b@live.com>
2026-09-17 15:37:05 +0800

    【修复&资源】修复弹幕界面中，弹幕出现、消失位置不正确的问题；更新骆驼音效

--- beat-tap-world/web-mobile/index.3251d.js
+++ beat-tap-world/web-mobile/index.3251d.js
@@ -1,4 +1,4 @@
-System.register(["./application.2be89.js"], function (_export, _context) {
+System.register(["./application.c96d6.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
