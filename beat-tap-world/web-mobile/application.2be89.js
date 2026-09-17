56960304c402a34b287003ed3d4f4710537b6abc
FuNing <funing0721b@live.com>
2026-09-17 11:57:41 +0800

    新增加载界面

--- beat-tap-world/web-mobile/application.2be89.js
+++ beat-tap-world/web-mobile/application.2be89.js
@@ -14,7 +14,7 @@ System.register([], function (_export, _context) {
       _export("Application", Application = /*#__PURE__*/function () {
         function Application() {
           _classCallCheck(this, Application);
-          this.settingsPath = 'src/settings.91731.json';
+          this.settingsPath = 'src/settings.21ce0.json';
           this.showFPS = false;
         }
         _createClass(Application, [{
