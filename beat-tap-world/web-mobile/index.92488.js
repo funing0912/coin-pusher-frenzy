22253cf0d225a94d91bcd76cca3946f06e3ad06c
FuNing <funing0721b@live.com>
2026-09-21 14:36:08 +0800

    更新开合扇关卡 chart.json

--- beat-tap-world/web-mobile/index.92488.js
+++ beat-tap-world/web-mobile/index.92488.js
@@ -1,4 +1,4 @@
-System.register(["./application.971f7.js"], function (_export, _context) {
+System.register(["./application.7d5f3.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
