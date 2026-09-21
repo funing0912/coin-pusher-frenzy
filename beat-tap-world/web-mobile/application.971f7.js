9bffb67f1d32f376c6ac832d6db4e2c0420c6935
FuNing <funing0721b@live.com>
2026-09-21 13:33:55 +0800

    新增开合扇音效；修改骆驼准备、吐的音效

--- beat-tap-world/web-mobile/application.971f7.js
+++ beat-tap-world/web-mobile/application.971f7.js
@@ -14,7 +14,7 @@ System.register([], function (_export, _context) {
       _export("Application", Application = /*#__PURE__*/function () {
         function Application() {
           _classCallCheck(this, Application);
-          this.settingsPath = 'src/settings.b36f0.json';
+          this.settingsPath = 'src/settings.fba01.json';
           this.showFPS = false;
         }
         _createClass(Application, [{
