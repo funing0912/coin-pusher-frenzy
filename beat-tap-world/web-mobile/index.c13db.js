7354aab4c90b0400bd900fa4009e79a1d2f8b540
FuNing <funing0721b@live.com>
2026-09-24 22:01:42 +0800

    屏幕干扰效果：灰度控制；扇子开合按钮样式；扇子灯光遮罩配置

--- beat-tap-world/web-mobile/index.c13db.js
+++ beat-tap-world/web-mobile/index.c13db.js
@@ -1,4 +1,4 @@
-System.register(["./application.7d5f3.js"], function (_export, _context) {
+System.register(["./application.d5109.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
